288888a8dffb31f14f4d586ea9d54483a684b58f
ada450 <adrienbelegaud2@gmail.com>
2025-12-09 17:54:12 +0100

    Test map with Plotly on the browser

--- core/views.py
+++ core/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import plotly as plt
+import seaborn as sns
+from urllib.request import urlopen
+import json
+import plotly.express as px
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+import pandas as pd
 
 def home(request):
+    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+    dtype={"fips": str})
+
+
+
+    fig = px.choropleth_map(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           map_style="carto-positron",
+                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           labels={'unemp':'unemployment rate'}
+                          )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
     return HttpResponse("Hello world!")
+
+
+
+
